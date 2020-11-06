16e99ef9189930c60aa2c7693c92370068f2619b
daniil789 <pechenkin031199@mail.ru>
2020-11-06 22:47:42 +0500

    task 1,2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,7 +6,70 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите размерность массива");
+
+            int n;
+
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                int[] array = CreateRandomArray(n);
+                ShowArray(array);
+                Console.WriteLine($"Номер минимального элемента = {FindIndexMin(array)}");
+            }
+            catch
+            {
+                Error();
+            }
+        }
+
+        static void Error()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Введены некорректные данные");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static int[] CreateRandomArray(int n)
+        {
+            int[] array = new int[n];
+
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 20);
+
+            }
+
+            return array;
+        }
+
+        static int[] ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i} - {array[i]}");
+
+            }
+
+            return array;
+        }
+
+        static int FindIndexMin(int[] array)
+        {
+            int min = int.MaxValue;
+            int indexMinElement = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    indexMinElement = i;
+                }
+            }
+            return indexMinElement;
         }
     }
 }
